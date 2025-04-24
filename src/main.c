32b5d30aff47f88f703dda3e05435b750d3fc167
Ernest Balana Gelpi <ebalana-@cbr6s4.42barcelona.com>
2025-04-24 15:34:23 +0200

    almost done, still problem with single and double quotes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ebalana- <ebalana-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 13:40:15 by mavellan          #+#    #+#             */
-/*   Updated: 2025/04/23 17:35:29 by ebalana-         ###   ########.fr       */
+/*   Updated: 2025/04/23 19:44:16 by ebalana-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,38 +29,6 @@
 // 		free(line);
 // 	}
 // 	return 0;
-// }
-
-// int	main(void)
-// {
-// 	char	*line;
-// 	char	**tokens;
-// 	int		i;
-
-// 	while (1)
-// 	{
-// 		line = readline("minishell$ ");
-// 		if (!line)
-// 			break ;
-// 		if (*line)
-// 		{
-// 			add_history(line);
-// 			tokens = tokenize_input(line);
-// 			if (tokens)
-// 			{
-// 				i = 0;
-// 				while (tokens[i])
-// 				{
-// 					printf("Token[%d]: %s\n", i, tokens[i]);
-// 					free(tokens[i]);
-// 					i++;
-// 				}
-// 				free(tokens);
-// 			}
-// 		}
-// 		free(line);
-// 	}
-// 	return (0);
 // }
 
 int	main(void)
