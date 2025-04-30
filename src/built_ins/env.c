fa73bae503a55718da9646553c5486dff4c1b128
Ernest Balana Gelpi <ebalana-@cbr6s4.42barcelona.com>
2025-04-30 13:56:54 +0200

    built in |  unset

--- src/built_ins/env.c
+++ src/built_ins/env.c
@@ -6,7 +6,7 @@
 /*   By: ebalana- <ebalana-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 12:33:10 by ebalana-          #+#    #+#             */
-/*   Updated: 2025/04/30 13:08:12 by ebalana-         ###   ########.fr       */
+/*   Updated: 2025/04/30 13:52:17 by ebalana-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,4 +65,45 @@ int	ft_env(t_env *env_list)
 		env_list = env_list->next;
 	}
 	return (0);
+}
+
+int	is_valid_identifier(const char *str)
+{
+	int	i;
+
+	if (!str || !str[0] || (!ft_isalpha(str[0]) && str[0] != '_'))
+		return (0);
+	i = 1;
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	remove_env_key(t_env **env_list, const char *key)
+{
+	t_env	*curr;
+	t_env	*prev;
+
+	curr = *env_list;
+	prev = NULL;
+	while (curr)
+	{
+		if (ft_strcmp(curr->key, key) == 0)
+		{
+			if (prev)
+				prev->next = curr->next;
+			else
+				*env_list = curr->next;
+			free(curr->key);
+			free(curr->value);
+			free(curr);
+			return ;
+		}
+		prev = curr;
+		curr = curr->next;
+	}
 }
