a33e75d5651d4d44c144417e61f20e056d67c50f
Miquel Avellaneda <mavellanedal2223@politecnics.barcelona>
2025-05-13 19:38:25 +0200

    Changed ft_free declaration in librf.h

--- lib/libft/libft.h
+++ lib/libft/libft.h
@@ -53,7 +53,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);void	ft_free(char **list);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_free(char **list);
 char	*ft_strcat(char *dest, char *src);
 char	*ft_strcpy(char *dest, char *src);
 
